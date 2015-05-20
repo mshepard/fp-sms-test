3230becc5511166770b38d565c969eff1f957d33
Mark Shepard <markshepard@diabolo-verde.local>
2015-05-20 12:18:54 -0400

    implement list subscribers dynamic

--- controllers/pages.js
+++ controllers/pages.js
@@ -9,10 +9,9 @@ exports.showForm = function(request, response) {
 // Render subscriber list
 exports.listSubscribers = function(request, response) {
     // Render form, with any success or error flash messages
-	var items = Subscriber.listAll();
     response.render('subscribers', {
         errors: request.flash('errors'),
         successes: request.flash('successes'),
-		items: items
+		items: Subscriber.listAll();
     });
 };
